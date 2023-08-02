53075bd7d912e15fa026b1de0c66946ae7d029cd
Tiaguh <tiagosingup@gmail.com>
2023-08-01 22:57:21 -0300

    fixing login bugs '-''

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,15 +1,15 @@
 import database from '../repository/connection.js';
 
 async function Login(email, password) {
-  const sql = "SELECT * FROM Admin WHERE email = ?;";
+  const sql = "SELECT * FROM Admin WHERE email = ? AND password = ?;";
   const data = [email, password];
 
   const conn = await database.connect();
   const [rows] = await conn.query(sql, data);
-  
+
   conn.end();
-  
+
   return rows;
 }
 
-export default { Login }
+export default { Login };
