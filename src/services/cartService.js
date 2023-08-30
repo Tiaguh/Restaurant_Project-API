5df0c932875e93ffcb6818defbdf537824a7b64d
Tiaguh <tiagosingup@gmail.com>
2023-08-30 16:15:34 -0300

    changing query again ;-;

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -57,32 +57,26 @@ async function addItemCart(user_id, item_id) {
 // Obtém todos os itens no carrinho de um usuário, incluindo a quantidade de cada item.
 async function getCartItems(user_id) {
     const sql = `
-      SELECT
+    SELECT
         M.id,
         M.description,
-        M.name AS item_name,
-        C.quantity AS quantity,
-        C.quantity * M.price AS total_price
-      FROM
+        M.name,
+        C.quantity
+    FROM
         Cart AS C
-      JOIN
+    JOIN
         Menu AS M ON C.item_id = M.id
-      WHERE
-        C.user_id = ?;
+    WHERE
+        C.user_id = ?;  
     `;
+
     const data = [user_id];
 
     const conn = await database.connect();
     const [rows] = await conn.query(sql, data);
     conn.end();
-
-    // Convertendo o total_price de string para número inteiro
-    const cartItems = rows.map(item => ({
-        ...item,
-        total_price: parseInt(item.total_price)
-    }));
-
-    return cartItems;
+    
+    return rows;
 }
 
 // Remove um item do carrinho de um usuário.
