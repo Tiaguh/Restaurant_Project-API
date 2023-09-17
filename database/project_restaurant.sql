f54ab07afd8175a6ab9628cac613eee802551192
Tiaguh <tiagosingup@gmail.com>
2023-09-17 15:58:53 -0300

    update database

--- database/project_restaurant.sql
+++ database/project_restaurant.sql
@@ -4,24 +4,26 @@ USE Restaurant_Project;
 
 -- DROP DATABASE Restaurant_Project;
 
+-- Tabela que guarda os itens que o cliente pode comprar, bem como descrição e preço do produto.
 CREATE TABLE Menu (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(45) NOT NULL,
     description VARCHAR(400) NOT NULL,
-    price DECIMAL(5,2) NOT NULL,
+    price DECIMAL(5, 2) NOT NULL,
     img VARCHAR(255),
-    
     PRIMARY KEY(id)
 );
 
+
+-- Guarda as informações do usuário, como email e senha.
 CREATE TABLE User (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    address VARCHAR(250)
 );
 
+-- Guarda informações para administradores poderem logar.
 CREATE TABLE Admin (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
@@ -29,28 +31,28 @@ CREATE TABLE Admin (
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Purchase (
+-- Guarda os pedidos dos usuários.
+CREATE TABLE Requests (
     id INT NOT NULL AUTO_INCREMENT,
-    methodo_pay VARCHAR(50) NOT NULL,
-    quantity INT NOT NULL,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date DATE DEFAULT CURRENT_DATE,
+    hour TIME DEFAULT CURRENT_TIME,
     item_id INT NOT NULL,
     user_id INT NOT NULL,
-        
+    quantity INT NOT NULL,
     PRIMARY KEY(id),
     
-    CONSTRAINT FK_item_purchase FOREIGN KEY(item_id) REFERENCES Menu(id),
-    CONSTRAINT FK_user_purchase FOREIGN KEY(user_id) REFERENCES User(id)
+    CONSTRAINT FK_item_request FOREIGN KEY(item_id) REFERENCES Menu(id),
+    CONSTRAINT FK_user_request FOREIGN KEY(user_id) REFERENCES User(id)
 );
 
+-- Armazena os itens que o usuário coloca no carrinho.
 CREATE TABLE Cart (
     id INT NOT NULL AUTO_INCREMENT,
     item_id INT NOT NULL,
     user_id INT NOT NULL,
     quantity INT NOT NULL DEFAULT '1',
-    
     PRIMARY KEY(id),
-    
+
     CONSTRAINT FK_cart_item FOREIGN KEY(item_id) REFERENCES Menu(id),
     CONSTRAINT FK_cart_user FOREIGN KEY(user_id) REFERENCES User(id)
 );
