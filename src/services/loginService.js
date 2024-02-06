30a703fea3bf2370ca64ff58d13a2765fffd7f4d
Tiaguh <tiagosingup@gmail.com>
2024-02-05 22:09:04 -0300

    removing bcrypt

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,5 +1,4 @@
 import database from '../repository/connection.js';
-import bcrypt from 'bcrypt';
 
 async function LoginAdmin(email, password) {
   const sql = "SELECT * FROM Admin WHERE email = ? AND password = ?;";
@@ -14,34 +13,15 @@ async function LoginAdmin(email, password) {
 }
 
 async function LoginClient(email, password) {
-  const sql = "SELECT * FROM User WHERE email = ?;";
-  const data = [email];
+  const sql = "SELECT * FROM User WHERE email = ? AND password = ?;";
+  const data = [email, password];
 
   const conn = await database.connect();
   const [rows] = await conn.query(sql, data);
 
   conn.end();
 
-  if (rows.length > 0) {
-    const user = rows[0];
-    const hashedPassword = user.password;
-
-    const isPlainTextPassword = !/^\$2[aby]\$[0-9]+\$/.test(hashedPassword);
-
-    let passwordMatch;
-
-    if (isPlainTextPassword) {
-      passwordMatch = await bcrypt.compare(password, hashedPassword);
-    } else {
-      passwordMatch = await bcrypt.compare(password, hashedPassword);
-    }
-
-    if (passwordMatch) {
-      return [user];
-    }
-  }
-
-  return [];
+  return rows;
 }
 
 export default { LoginAdmin, LoginClient };
